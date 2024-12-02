24534615811530aca92423acbea7479194f33522
Raghav Shiwakoti <Raghav.Shiwakoti@coyotes.usd.edu>
2024-12-01 21:41:26 -0600

    Update Autori.py

--- Autori/Autori.py
+++ Autori/Autori.py
@@ -1,6 +1,7 @@
-x=[*input().split()]
-print("x[0]",end="")
-for i in x:
-    if i== "-"
-      t+=s[i+1]
-print(t,end="")      
+x=input()
+t = x[0]
+for i in  range(len(x)):
+    if x[i]== "-":
+        t+= x[i+1]
+print(t)
+       
