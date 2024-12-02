924b189c038b96a1400aabc816db9b9ecddb6f77
Raghav Shiwakoti <Raghav.Shiwakoti@coyotes.usd.edu>
2024-12-01 20:38:35 -0600

    Update Barcelona.py

--- Barcelona/Barcelona.py
+++ Barcelona/Barcelona.py
@@ -1,2 +1,2 @@
-a1,a2=[int(c) for c in input().split()],[int(c) for c in input().split()]
+a1,a2=[*input().split()],[*input().split()]
 print("fyrst" if (p:=a2.index(a1[1])+1)==1 else "naestfyrst" if p==2 else f"{p} fyrst")
